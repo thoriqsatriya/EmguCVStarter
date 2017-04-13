cb2f820fae54af04530ee47ca851ea3622fc60b5
thoriqsatriya <thoriq.satriya94@gmail.com>
2017-04-13 11:29:24 +0700

    add new project

---
+++ EmguCVStarter/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace EmguCVStarter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.imageBox3 = new Emgu.CV.UI.ImageBox();
+            this.imageBox4 = new Emgu.CV.UI.ImageBox();
+            this.labelTittle1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonProcess = new System.Windows.Forms.Button();
+            this.buttonLoadVideo = new System.Windows.Forms.Button();
+            this.radioButtonImage = new System.Windows.Forms.RadioButton();
+            this.radioButtonVideo = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox1.Location = new System.Drawing.Point(26, 38);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(320, 240);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox2.Location = new System.Drawing.Point(373, 38);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(320, 240);
+            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
+            // 
+            // imageBox3
+            // 
+            this.imageBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox3.Location = new System.Drawing.Point(26, 349);
+            this.imageBox3.Name = "imageBox3";
+            this.imageBox3.Size = new System.Drawing.Size(320, 240);
+            this.imageBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox3.TabIndex = 4;
+            this.imageBox3.TabStop = false;
+            // 
+            // imageBox4
+            // 
+            this.imageBox4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox4.Location = new System.Drawing.Point(373, 349);
+            this.imageBox4.Name = "imageBox4";
+            this.imageBox4.Size = new System.Drawing.Size(320, 240);
+            this.imageBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox4.TabIndex = 5;
+            this.imageBox4.TabStop = false;
+            // 
+            // labelTittle1
+            // 
+            this.labelTittle1.AutoSize = true;
+            this.labelTittle1.Location = new System.Drawing.Point(23, 22);
+            this.labelTittle1.Name = "labelTittle1";
+            this.labelTittle1.Size = new System.Drawing.Size(74, 13);
+            this.labelTittle1.TabIndex = 13;
+            this.labelTittle1.Text = "Original Image";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(370, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Processed Image 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 333);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Processed Image 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(370, 333);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Processed Image 3";
+            // 
+            // buttonProcess
+            // 
+            this.buttonProcess.Enabled = false;
+            this.buttonProcess.Location = new System.Drawing.Point(271, 284);
+            this.buttonProcess.Name = "buttonProcess";
+            this.buttonProcess.Size = new System.Drawing.Size(75, 23);
+            this.buttonProcess.TabIndex = 25;
+            this.buttonProcess.Text = "Process";
+            this.buttonProcess.UseVisualStyleBackColor = true;
+            this.buttonProcess.Click += new System.EventHandler(this.buttonProcess_Click);
+            // 
+            // buttonLoadVideo
+            // 
+            this.buttonLoadVideo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLoadVideo.Location = new System.Drawing.Point(170, 284);
+            this.buttonLoadVideo.Name = "buttonLoadVideo";
+            this.buttonLoadVideo.Size = new System.Drawing.Size(82, 23);
+            this.buttonLoadVideo.TabIndex = 26;
+            this.buttonLoadVideo.Text = "Load";
+            this.buttonLoadVideo.UseVisualStyleBackColor = true;
+            this.buttonLoadVideo.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // radioButtonImage
+            // 
+            this.radioButtonImage.AutoSize = true;
+            this.radioButtonImage.Checked = true;
+            this.radioButtonImage.Location = new System.Drawing.Point(744, 67);
+            this.radioButtonImage.Name = "radioButtonImage";
+            this.radioButtonImage.Size = new System.Drawing.Size(54, 17);
+            this.radioButtonImage.TabIndex = 27;
+            this.radioButtonImage.TabStop = true;
+            this.radioButtonImage.Text = "Image";
+            this.radioButtonImage.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonVideo
+            // 
+            this.radioButtonVideo.AutoSize = true;
+            this.radioButtonVideo.Location = new System.Drawing.Point(818, 67);
+            this.radioButtonVideo.Name = "radioButtonVideo";
+            this.radioButtonVideo.Size = new System.Drawing.Size(52, 17);
+            this.radioButtonVideo.TabIndex = 28;
+            this.radioButtonVideo.Text = "Video";
+            this.radioButtonVideo.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(741, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Processing Type";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 601);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButtonVideo);
+            this.Controls.Add(this.radioButtonImage);
+            this.Controls.Add(this.buttonLoadVideo);
+            this.Controls.Add(this.buttonProcess);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelTittle1);
+            this.Controls.Add(this.imageBox4);
+            this.Controls.Add(this.imageBox3);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public Emgu.CV.UI.ImageBox imageBox1;
+        public Emgu.CV.UI.ImageBox imageBox2;
+        public Emgu.CV.UI.ImageBox imageBox3;
+        public Emgu.CV.UI.ImageBox imageBox4;
+        private System.Windows.Forms.Label labelTittle1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonProcess;
+        private System.Windows.Forms.Button buttonLoadVideo;
+        private System.Windows.Forms.RadioButton radioButtonImage;
+        private System.Windows.Forms.RadioButton radioButtonVideo;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ EmguCVStarter/Form1.cs
@@ -0,0 +1,132 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EmguCVStarter
+{
+    public partial class Form1 : Form
+    {
+        Mat originalImage = new Mat();
+        Mat grayscaleImage = new Mat();
+        Mat processedImage1 = new Mat();
+        Mat processedImage2 = new Mat();
+        Mat processedImage3 = new Mat();
+
+        private Capture capture = null;
+        int FPS = 25;
+        Timer myTimer = new Timer();
+        bool isPlaying = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// a function to process video frame
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ProcessVideoFrame(object sender, EventArgs e)
+        {
+            //stream video & convert
+            originalImage = capture.QueryFrame();
+            try
+            {
+                CvInvoke.CvtColor(originalImage, grayscaleImage, ColorConversion.Bgr2Gray);
+                Process();
+            }
+            catch (NullReferenceException ex)
+            {
+                isPlaying = false;
+                myTimer.Stop();
+                capture.Stop();
+                MessageBox.Show("End of video file or " + ex.Message);
+            }
+        }
+
+        //this function is called when "Load" button is clicked
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            if (Openfile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (radioButtonImage.Checked)
+                    {
+                        grayscaleImage = CvInvoke.Imread(Openfile.FileName, LoadImageType.Grayscale);
+                        buttonProcess.Enabled = true;
+                        Process();
+                    }
+                    else if (radioButtonVideo.Checked)
+                    {
+                        capture = new Capture(Openfile.FileName);
+                        myTimer.Interval = 1000 / FPS;
+                        myTimer.Tick += new EventHandler(ProcessVideoFrame);
+                        buttonProcess.Enabled = true;
+                        imageBox1.Image = null;
+                        imageBox2.Image = null;
+                        imageBox3.Image = null;
+                        imageBox4.Image = null;
+                    }
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+                catch (ArgumentException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        //this function is called when "Process" button is clicked
+        private void buttonProcess_Click(object sender, EventArgs e)
+        {
+            if (radioButtonVideo.Checked)
+            {
+                if (!isPlaying)
+                {
+                    isPlaying = !isPlaying;
+                    myTimer.Start();
+                    buttonProcess.Text = "Stop";
+                }
+                else
+                {
+                    isPlaying = !isPlaying;
+                    buttonProcess.Text = "Start";
+                    myTimer.Stop();
+                    capture.Stop();
+                }
+            }
+            else if (radioButtonImage.Checked)
+            {
+                Process();
+            }
+        }
+
+        //main process of image processing 
+        private void Process()
+        {
+            imageBox1.Image = grayscaleImage;
+
+            CvInvoke.Threshold(grayscaleImage, processedImage1, 40, 255, ThresholdType.Binary);
+            imageBox2.Image = processedImage1;
+
+            CvInvoke.Canny(grayscaleImage, processedImage2, 30, 70);
+            imageBox3.Image = processedImage2;
+
+            CvInvoke.CLAHE(grayscaleImage, 40, new Size(8, 8), processedImage3);
+            imageBox4.Image = processedImage3;
+        }
+    }
+}
